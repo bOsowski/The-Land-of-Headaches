645248408ed2b7b07f2e8c16e0676f70a98bc641
Bartosz Osowski <bosowski04@gmail.com>
2018-12-10 05:30:00 +0000

    Fixed a race condition in which assets sometimes would fail to load.

--- The Land of Headaches/AssetManager.cpp
+++ The Land of Headaches/AssetManager.cpp
@@ -10,31 +10,32 @@ AssetManager::AssetManager() {
     DIR *dir;
     struct dirent *ent;
 
-    const char* textureDir = ((resourcePath()+"textures/").c_str());
-    const std::string texturesPath = std::string(textureDir);
+    bool fail = true;
+    while(fail){
+        const char* textureDir = ((resourcePath()+"textures/").c_str());
+        const std::string texturesPath = std::string(textureDir);
 
-    if ((dir = opendir (textureDir)) != NULL) {
-        std::cout<< "Found textures folder."<<std::endl;
-        while ((ent = readdir (dir)) != NULL) {
-            std::string fileName = std::string(ent->d_name);
+        if ((dir = opendir (textureDir)) != NULL) {
+            std::cout<< "Found textures folder."<<std::endl;
+            while ((ent = readdir (dir)) != NULL) {
+                std::string fileName = std::string(ent->d_name);
 
-            if(fileName == "." || fileName == "..")continue;
+                if(fileName == "." || fileName == "..")continue;
 
-            std::string full_path = texturesPath + fileName;
-            std::cout << "Trying to load from " + full_path << std::endl;
-            sf::Texture texture;
-            if ( texture.loadFromFile(full_path)) {
-                textures.insert(std::pair<std::string, sf::Texture>(fileName, texture));
-                std::cout << "Loaded '" + fileName << "'" << std::endl<<std::endl;
+                std::string full_path = texturesPath + fileName;
+                std::cout << "Trying to load from " + full_path << std::endl;
+                sf::Texture texture;
+                if ( texture.loadFromFile(full_path)) {
+                    textures.insert(std::pair<std::string, sf::Texture>(fileName, texture));
+                    std::cout << "Loaded '" + fileName << "'" << std::endl<<std::endl;
+                }
+                else{
+                    std::cout << "Failed to load '" + fileName << "'" << std::endl<<std::endl;
+                }
             }
-            else{
-                std::cout << "Failed to load '" + fileName << "'" << std::endl<<std::endl;
-            }
+            closedir (dir);
+            fail = false;
         }
-        closedir (dir);
     }
-    else {
-        /* could not open directory */
-        perror ("Failed to open resources.");
-    }
+
 }
